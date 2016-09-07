f3a21d57ab9503e9b4b7df9b6279bbd2a28f2723
RBroden <robertbroden@outlook.com>
2016-09-07 07:25:48 -0400

    Message.html/js Changes

---
+++ messages.js
@@ -0,0 +1,9 @@
+(function(angular) {
+   'use strict';
+   angular.module('nlsChatApp', [])
+   .controller('messagesCtrl', ['$scope', function($scope, $http) {
+
+      $scope.a = 5;
+
+   }]);
+})(window.angular);
